0c575547eacb56c812e9868425e63d1391e6f49c
Leo Lou <leo.lou.43@gmail.com>
2015-10-01 18:40:16 +1000

    Create LCD_disp.c

---
+++ Code/Commander/LCD/LCD_disp.c
@@ -0,0 +1,47 @@
+
+/*
+Authored by Leo Lou
+Dated 1 October 2015
+********************************************************************************
+This function takes in an number (as outlined in serialcommand.c) that refers to both a corresponding
+string output and value output to the LCD
+
+main function has been kept for testing purposes. The variable 'x' corresponds to menu_ref_1 from my previous
+file. Further improvements could involve a look up table.
+
+The function of this program depends on an agreed upon indexing system of variables and strings (to be discussed
+later)
+*/
+
+
+#include  <stlib.h>
+#include  <p18f452.h>
+#define LCDL 16
+
+int pid_gain=50;
+int max_speed=20;
+int max_yaw=30;
+int ir_samp_e=60;
+const char s0[LCDl]="PID gain:";
+const char s1[LCDl]="Max speed:";
+const char s2[LCDl]="Max yaw:";
+const char s3[LCDl]="IR samp/est:";
+    
+const char *stringtab[4]={s0,s1,s2,s3};
+
+int main(void)
+{
+    void LCD_disp(int x);
+    LCD_disp(2);
+    return 0;
+}
+
+void LCD_disp(int x)
+{
+
+    int values[4]={pid_gain,max_speed,max_yaw,ir_samp_e};            
+    char  string[LCDl];
+    sprintf(string, "%s %d\n", stringtab[x],values[x]);
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String(string1);
+}
