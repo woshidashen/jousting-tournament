a6723d04e6c9e3fcf92e325f061d3c7a2f77ead5
Lydia-D <lydia.drabsch@gmail.com>
2015-10-04 01:22:37 +1000

    Initial Turning Code
    Turning for a set velocity using POT as turning value. 128 is no turn, 0
    if full left turn, 255 is full right turn. Turn rate is decided as
    percentage from maximum turn rate at current velocity. Built, not
    tested.

---
+++ Code/Robot/MotorDriverC.X/PWM_motor.c
@@ -0,0 +1,106 @@
+/* 
+ * File:   PWM_motor.c
+ * Author: ldra3_000
+ *
+ * Created on 29 September 2015, 8:08 PM
+ * Location: Robot
+ * Resources: Timer2, CCP1 - PORTC<2>, CCP2 - PORTC<1>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p18f4520.h>
+#include "configReg4520.h"
+
+#define bit(x) (1<<(x)) 
+#define test_bit(var,pos) ((var) & (1<<(pos)))
+//#define change_bit(var,pos,val) ()
+//number ^= (-x ^ number) & (1 << n);
+
+unsigned int L_PWM = 50;  // value between 0 (0 duty cycle) and FF (100 duty cycle)
+unsigned int L_dir = 1; // 1 is fwd, 0 is back
+unsigned int L_inputA = 0;
+unsigned int L_inputB = 0;
+unsigned int R_PWM = 50;
+unsigned int R_dir = 1;
+unsigned int R_inputA = 0;
+unsigned int R_inputB = 0;
+unsigned int Motor_enable = 0; // 1 is enabled, both motors are either enabled or disabled
+unsigned int bit_1A = 0; // FOR MOTORS of PORT C
+unsigned int bit_1B = 3;
+unsigned int bit_2A = 4;
+unsigned int bit_2B = 5;
+unsigned int bit_enable = 3;
+unsigned int length_wheels = 15; // length between wheels cm
+unsigned int vel = 128; // 'velocity' associated with 50% duty cycle
+unsigned int max_Dvx;
+
+void PWM_motor_setup(void);
+void Inputs_motor_setup(void);
+void ADC_setup(void);
+void direction(void);
+void turn(unsigned int);
+
+
+void PWM_motor_setup(void){
+    PR2 = 0xFF;         // set period (FF = 610Hz with timer prescaler 16)
+    T2CON = 0x07;  // Sets bit 2 to enable timer 2(TMR2), prescaler 16
+    CCPR2L = L_PWM;   // select duty cycle
+    CCPR1L = R_PWM;   // select duty cycle
+    TRISC =0; // output for CCP1-RC<2> and CCP2-RC<1>
+    CCP2CON = 0x0C; // Configures CCP2 for PWM mode (CCP2M2 and CCP2M3)
+    CCP1CON = 0X0C;     
+}
+
+void Inputs_motor_setup(void){
+    TRISB &=~bit(bit_enable);
+    PORTB |=  (Motor_enable<<bit_enable) ;   // shift motor_enable left by 3 bits
+    direction();
+    PORTC |= (R_inputA<<bit_1A)|(R_inputB<<bit_1B)|(L_inputA<<bit_2A)|(L_inputB<<bit_2B); // set direction of wheels
+}
+
+void ADC_setup(void){
+    T0CON = 0xC7; //  11000111  Enables TMR0, 8bit with 256 prescale (p103)
+    ADCON0 = 0x41;  //0100 0001 Fosc/8, A/D enabled
+    ADCON1 = 0x0E;  // 0000 1110 Make RA0 analog input, Left justify, 1 analog channel
+}
+
+void direction(void){
+    //number ^= (-x ^ number) & (1 << n);
+    L_inputA = L_dir|(bit(0)^L_dir) ; // toggle bit
+    L_inputB = L_dir;
+    R_inputA = R_dir;
+    R_inputB = R_dir|(bit(0)^R_dir) ;// toggle bit
+
+}
+// update PWM values
+void turn(unsigned int POT_val){
+    // 128 is middle, which is no turn
+    if (vel<=128){
+        max_Dvx = vel;
+    }else if (vel> 128){
+        max_Dvx = 255-vel;
+    }
+    L_PWM = (POT_val-128)*max_Dvx/128;
+    R_PWM = 2*vel-L_PWM;
+    CCPR1L = R_PWM;
+    CCPR2L = L_PWM;
+}
+
+
+
+void main(void){
+    PWM_motor_setup();
+    Inputs_motor_setup();
+    ADC_setup();
+
+    
+    for(;;){
+    while(test_bit(INTCON, INTCONbits.TMR0IF)==0){}   // busy loop
+    ADCON0 |= bit(ADCON0bits.GO); 
+    while(test_bit(PIR1, PIR1bits.ADIF)==0){}   // busy loop
+    
+    turn(ADRESH);  // does the 8 bit to unsigned int work?
+    
+    }
+}
---
+++ Code/Robot/MotorDriverC.X/build/default/production/PWM_motor.o.d
@@ -0,0 +1,4 @@
+build/default/production/PWM_motor.o:  \
+	C:/Users/ldra3_000/Documents/UNI/MTRX3700/MotorDriverC.X/PWM_motor.c  \
+	C:/Users/ldra3_000/Documents/UNI/MTRX3700/MotorDriverC.X/configReg4520.h  \
+
